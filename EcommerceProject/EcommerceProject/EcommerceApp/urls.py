0696906308f8ddf49c919a54a4188410ad2fed92
Георги Йорданов <gyordanov94@abv.bg>
2023-08-23 16:33:38 +0300

    add category.html and CategoryView

--- EcommerceProject/EcommerceProject/EcommerceApp/urls.py
+++ EcommerceProject/EcommerceProject/EcommerceApp/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from EcommerceProject.EcommerceApp.views import home
+from EcommerceProject.EcommerceApp.views import home, CategoryView
 
-urlpatterns = (
+urlpatterns = [
     path('', home, name='home'),
-)
+    path('category/<slug:val>', CategoryView.as_view(), name='category'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EcommerceProject/EcommerceProject/EcommerceApp/views.py
+++ EcommerceProject/EcommerceProject/EcommerceApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from EcommerceProject.EcommerceApp.models import Product
 
 
 def home(request):
     return render(request, "EcommerceApp/home.html")
+
+
+class CategoryView(View):
+    def get(self, request, val):
+        products = Product.objects.filter(category=val)
+        return render(request, "EcommerceApp/category.html", locals())
