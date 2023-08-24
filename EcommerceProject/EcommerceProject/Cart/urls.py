f7269a1c293f71704c75b6236ca82515aceb7871
Георги Йорданов <gyordanov94@abv.bg>
2023-08-25 01:34:15 +0300

    add add to cart logic

--- EcommerceProject/EcommerceProject/Cart/urls.py
+++ EcommerceProject/EcommerceProject/Cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from EcommerceProject.Cart.views import add_to_cart
+from EcommerceProject.Cart.views import add_to_cart, show_cart, checkout
 
 urlpatterns = [
     path('add-to-cart/', add_to_cart, name='add-to-cart'),
-    # path('cart/', show_cart, name='show-cart'),
+    path('cart/', show_cart, name='show-cart'),
+    path('checkout/', show_cart, name='checkout'),
 
 ]
--- EcommerceProject/EcommerceProject/Cart/views.py
+++ EcommerceProject/EcommerceProject/Cart/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from EcommerceProject.Cart.models import Cart
+from EcommerceProject.EcommerceApp.models import Product
 
 
 def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    # product = Product.objects.filter(id=product_id).get()
+    product = get_object_or_404(Product, id=product_id)
+    Cart(user=user, product=product).save()
+
+    return redirect("/cart")
+
+
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    amount = 0
+    for p in cart:
+        value = p.quantity * p.product.discounted_price
+        amount = amount + value
+    totalamount = amount + 40
+    return render(request, 'Cart/addtocart.html', locals())
+
+
+def checkout(request):
     pass
