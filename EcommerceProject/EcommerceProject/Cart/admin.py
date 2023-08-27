52801e3df24acf40631c1d497b8f45952d40d608
Георги Йорданов <gyordanov94@abv.bg>
2023-08-27 18:20:50 +0300

    add wishlist models

--- EcommerceProject/EcommerceProject/Cart/admin.py
+++ EcommerceProject/EcommerceProject/Cart/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from EcommerceProject.Cart.models import Cart
+from EcommerceProject.Cart.models import Cart, Wishlist
 
 
 @admin.register(Cart)
 class CustomerModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'product', 'quantity']
 
+
+@admin.register(Wishlist)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product']
